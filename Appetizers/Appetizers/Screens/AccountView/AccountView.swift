52fefe8e4c81af376e7c99b2ac4965ecff8786f8
nhuthq <nhuthq@zigexn.vn>
2024-08-28 10:34:11 +0700

    init personal section

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firtsName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthday = Date()
+    
     var body: some View {
-        NavigationStack{
-            Text("Account View!")
-                .navigationTitle("🚀  Account")
+        NavigationStack {
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name: ", text: $firtsName)
+                    TextField("Last Name: ", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.none)
+                    DatePicker("Birthday",
+                               selection: $birthday,
+                               displayedComponents: .date)
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+            }
+            .navigationTitle("🚀  Account")
         }
     }
 }
