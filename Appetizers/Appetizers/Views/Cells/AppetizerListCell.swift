7ed7c1d2533043016059a8df6b361350b9d0f05f
nhuthq <nhuthq@zigexn.vn>
2024-09-02 12:37:49 +0700

    add async image

--- Appetizers/Appetizers/Views/Cells/AppetizerListCell.swift
+++ Appetizers/Appetizers/Views/Cells/AppetizerListCell.swift
@@ -12,11 +12,22 @@ struct AppetizerListCell: View {
     let appetizer: Appetizer
     
     var body: some View {
-        HStack{
-            AppetizerRemoteImage(urlString: appetizer.imageURL)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 120, height: 90)
-                .cornerRadius(8)
+        HStack {
+            
+            AsyncImage(url: URL(string: appetizer.imageURL)) { image in
+                image
+                    .resizable()
+                    .frame(width: 120, height: 90)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            } placeholder: {
+                Image("food_placeholder")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120, height: 90)
+                    .cornerRadius(8)
+            }
+            
             VStack(alignment: .leading, spacing: 5 ){
                 Text(appetizer.name)
                     .font(.title2)
